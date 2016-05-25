98e621c0495fa72c5c2a9c187d5ff220054d9eb8
Jeff Kereakoglow <jkereako@users.noreply.github.com>
2016-05-25 16:17:21 -0400

    Organize files

---
+++ Source/AppDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.swift
+//  BlankState
+//
+//  Created by Jeffrey Kereakoglow on 9/11/15.
+//  Copyright © 2015 Alexis Digital. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  var window: UIWindow?
+}
